0f9dbca40a2d1d79e7700c645628251aea0bbbfa
Didier Marin <mail@didiermarin.com>
2021-10-31 16:23:56 +0100

    Fix build android script

--- build_android.sh
+++ build_android.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Build APK and copy it to assets -> http://localhost:8100/assets/app.apk
 
-npx cap copy android \
+rm -rf android \
+  && ionic capacitor build android \
   && cd android \
   && ./gradlew build \
   && cp -f app/build/outputs/apk/debug/app-debug.apk ../public/assets/app.apk
